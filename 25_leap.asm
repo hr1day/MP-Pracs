c50318eaf67691ab7d3da57b725a9021b14dfe03
Hriday Purohit <41243148+hr1day@users.noreply.github.com>
2019-11-06 10:40:32 +0530

    Update 25_leap.asm

--- 25_leap.asm
+++ 25_leap.asm
@@ -1,102 +1,126 @@
 Data Segment
-	msg1 db 10,13, "Enter Year: $"
-	year dw ?
-	msg2 db 10,13, "It is a Leap Year: $"
-	msg3 db 10,13, "It is Not a Leap Year: $"
+msg1 db 10,13,"Enter the year:$"
+msg2 db 10,13,"It is a leap year:$"
+msg3 db 10,13,"It is not a leap year:$"
+numh db ?
+numl db ?
+check db ?
 Data Ends
 Code Segment
+Assume CS:Code,DS:Data
+Start:
+	mov ax,data
+	mov ds,ax
 
-	Assume CS: Code, DS: Data
+	mov ah,09h
+	mov dx,offset msg1
+	int 21h
 
-	start:
+	call takenumber
+	mov numh,al
 
-		mov ax, data
-		mov ds, ax
+	call takenumber 
+	mov numl,al
 
-		mov dx, offset msg1
-		mov ah, 09h
-		int 21h
 
-		mov ah, 01h					; taking 1st digit
-		int 21h
-		Call input
-		mov ah, 00h
-		rol ax, 12
-		mov bx, ax
+	mov bl,04h
+	mov ah,numh
+	div bl
+	cmp ah,00h   ;Checking divisibility by 4
+	jz A         ;if divisible check divisibility by 100
 
-		mov ah, 01h					; taking 2nd digit
-		int 21h
-		Call input
-		mov ah, 00h
-		rol ax, 8
-		add bx, ax
+	mov ah,09h
+	mov dx,offset msg3
+	int 21h
+	call exit	 ;print not leap year
 
-		mov ah, 01h					; taking 3rd digit
-		int 21h
-		Call input
-		mov ah, 00h
-		rol ax, 4
-		add bx, ax
+	A:
+	mov bl,64h
+	mov ah,numh
+	mov al,numl
+	div bl
+    cmp ah,00h	;checking divisibility by 100
+    jz B        ;if it is divisible by 100 then check divisibility with 400
 
-		mov ah, 01h					; taking 4th digit
-		int 21h
-		Call input
-		mov ah, 00h
-		add bx, ax
+    mov ah,09h
+	mov dx,offset msg2
+	int 21h
+	call exit        ;print leap year if not divisible by 100
 
-		mov year, bx
 
-		mov ax, 0000H
-		mov al, bl
+    B:
+    mov bx,0190h     ;hex value for 400
+    mov ah,numh
+    mov al,numl
+    mov dx,00h
+    div bx
+    cmp dx,0000h
+    jz C
 
-		;Divide4:
 
-			mov bl, 04h
-			DIV bl					; bl is the divisor
-			mov bl, ah				; storing remainder in bl
+    mov ah,09h
+	mov dx,offset msg3
+	int 21h
+	call exit		;print leap year if not divisible by 400
 
-			cmp bl, 00h				; check divisiblity by 4
-			jz Leap
 
-			mov dx, offset msg3
-			mov ah, 09h
-			int 21h
-			jmp Exit
+    C:
+    mov ah,09h
+	mov dx,offset msg2
+	int 21h
+	call exit        ;print leap year if divisible by 400
 
-		Leap:
 
-			mov dx, offset msg2
-			mov ah, 09h
-			int 21h
 
-		Exit:
-			mov ah, 4ch
-			int 21h	
+	
 
-		input proc
-			cmp al, 41h
-			jc Inp
-			sub al, 07h
 
-			Inp:
-				sub al, 30h
 
-			ret
-			endp
 
-		output proc
-			cmp bl, 0ah
-			jc Outp
-			add bl, 07h
 
-			Outp:
-				add bl, 30h
 
-			mov dl, bl
-			mov ah, 02h
-			int 21h
+input proc
+mov ah,01h
+int 21h
+cmp al,41h
+jc laba
+sub al,07h
+laba:
+sub al,30h
+ret
+endp
 
-			ret
-			endp
+output proc
+cmp bl,0Ah
+jc labd
+add bl,07h
+labd:
+add bl,30h
+mov ah,02h
+mov dl,bl
+int 21h
+ret
+endp
+
+checkt proc
+inc check
+ret
+endp
+
+takenumber proc
+call input
+rol al,04h
+mov bl,al
+call input
+add al,bl
+ret
+endp
+
+exit proc
+mov ax,4C00h
+int 21h
+ret
+endp 
+
 Code Ends
-	end start
+ End Start
